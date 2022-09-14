0a81ac959c5154c885498fc0a41e85b59ed08512
Abe <69663458+AbeTGT@users.noreply.github.com>
2022-09-15 07:50:10 +1200

    Create VillagerCureEvent.java

---
+++ src/main/java/me.abetgt.villagercureevent/VillagerCureEvent.java
@@ -0,0 +1,30 @@
+package me.abetgt.villagercureevent;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityEvent;
+
+public final class VillagerCureEvent extends EntityEvent implements Cancellable {
+    public static final HandlerList handlers = new HandlerList();
+    private boolean cancel = false;
+
+    private Location eventLocationCured;
+
+    /**
+     * The VillagerCureEvent fires when a zombie villager turns into a normal villager after being cured.
+     * @param curedEntity The entity that was cured.
+     * @param locationCured The location where the entity was cured.
+     */
+    public VillagerCureEvent(Entity curedEntity, Location locationCured){
+        super(curedEntity);
+        eventLocationCured = locationCured;
+    };
+
+    /**
+     * Returns the location that the entity was cured at.
+     * @return Location (org.bukkit.Location) | Entity's location
+     */
+    public Location getLocation() {return eventLocationCured;}
+}
